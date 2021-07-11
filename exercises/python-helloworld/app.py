3e922843d294144465a26e2b549d6940e87eef40
Tok Xin Yi <tokxinyi@gmail.com>
2021-07-11 20:17:52 +0800

    Completed Lesson 2

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -8,30 +8,53 @@
 
 
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    logging.info('Main request successful')
     return "Hello World! Goodbye world!"
 
 @app.route("/status")
-def health_status():
-    if ("successful"):
-        return 'result: OK - healthy'
-    else:
-        return 'Error 500'
+# my attempt
+# def health_status():
+#     if ("successful"):
+#         return 'result: OK - healthy'
+#     else:
+#         return 'Error 500'
+
+def healthCheck():
+    response = app.response_class(
+        response = json.dumps({"result":"OK - healthy"}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    logging.info('Status request successful')
+    return response
 
 @app.route("/metrics")
+# my attempt
+# def metrics():
+#     data = {
+#         "user": "admin",
+#         "UserCount" : 140,
+#         "UserCountActive": 23
+#     }
+#    return data
+
 def metrics():
-    data = {
-        "user": "admin",
-        "UserCount" : 140,
-        "UserCountActive": 23
-    }
-
-    return data
-
-
+    response = app.response_class(
+        response = json.dumps({"status":"success", "code":0, "data":{"UserCount": 40, "UserCountActive": 23}}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    logging.info('Metrics request successful')
+    return response
 
 if __name__ == "__main__":
+    logging.basicConfig(filename = 'app.log', format='%(levelname)s:%(message)s', level=logging.DEBUG)
     app.run(host='0.0.0.0')
+    
